86dba44b5a1404d4cdc0433974d409d63e419f02
CarolRRod <carolina.rayrodrigues@gmail.com>
2026-03-29 19:09:58 +0100

    Create panel_builder.py

---
+++ data/panel_builder.py
@@ -0,0 +1,23 @@
+import pandas as pd 
+
+
+class PanelBuilder:
+    def __init__(self):
+        pass
+
+    def build_panel(self, data_dict):
+        frames = []
+
+        for ticker, df in data_dict:
+            temp = df.copy()
+            temp["Ticker"] = ticker
+            temp["Date"] = temp.index
+
+            frames.append(temp)
+
+        panel = pd.concat(frames)
+        panel = panel.reset_index(drop=True)
+        panel = panel.sort_values(["Date", "Ticker"])
+
+        return panel
+        
