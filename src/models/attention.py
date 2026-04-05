f08d353848a6c7072a9bbabb778f06d37d09a001
CarolRRod <carolina.rayrodrigues@gmail.com>
2026-04-05 18:54:24 +0100

    Add muliheadAttention

--- src/models/attention.py
+++ src/models/attention.py
@@ -36,3 +36,45 @@ class AttentionLSTM(nn.Module):
 
        out = self.fc(context)   # (batch_size, num_tickers)
        return out
+
+
+class MultiHeadAttentionLSTM(nn.Module):
+    def __init__(self,
+                 input_size,
+                 hidden_size=64,
+                 num_layers=2,
+                 num_heads=4,
+                 num_tickers=2):
+        super().__init__()
+
+        self.lstm = nn.LSTM(
+            input_size=input_size,
+            hidden_size=hidden_size,
+            num_layers=num_layers,
+            batch_first=True
+        )
+
+        self.attention = nn.MultiheadAttention(
+           embed_dim=hidden_size,
+           num_heads=num_heads,
+           batch_first=True 
+        )
+
+        self.fc = nn.Linear(hidden_size, num_tickers)
+
+    def forward(self, x):
+       """x: (batch_size, seq_len, input_size)"""
+
+       lstm_out, _ = self.lstm(x)   # (batch_size, seq_len, hidden_size)
+
+       attn_output, _ = self.attention(
+           lstm_out, lstm_out, lstm_out
+       )        # attn_out (batch_size, seq_len, hidden_size)
+
+       context = attn_output.mean(dim=1)    # (batch_size, hidden_size)
+       out = self.fc(context)               # (batch_size, num_tickers)
+
+       return out
+
+
+       
