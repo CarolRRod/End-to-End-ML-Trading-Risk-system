055a725dfac6070ced4af7faa4ede50e40364be1
CarolRRod <carolina.rayrodrigues@gmail.com>
2026-05-09 21:35:02 +0100

    update lstm

--- src/models/lstm.py
+++ src/models/lstm.py
@@ -1,31 +1,27 @@
 import torch
 import torch.nn as nn
 
-class MultiAssetLSTM(nn.Module):
+class LSTMModel(nn.Module):
     def __init__(self,
                  input_size,
                  hidden_size=64,
-                 proj_size=0,
-                 num_layers=1,
-                 num_tickers=1,
+                 num_layers=2,
+                 output_size=1,
                  bidirectional=False,
-                 batch_first=True
                 ):
         super().__init__()
 
         self.lstm = nn.LSTM(
                 input_size=input_size,
                 hidden_size=hidden_size,
-                proj_size=proj_size,
                 num_layers=num_layers,
                 bidirectional=bidirectional,
-                batch_first=batch_first
+                batch_first=True
             )
         
-        lstm_out_size = proj_size if proj_size > 0 else hidden_size
         direction_multiplier = 2 if bidirectional else 1
-
-        self.fc = nn.Linear(lstm_out_size * direction_multiplier, num_tickers)
+        self.norm = nn.LayerNorm(hidden_size * direction_multiplier)
+        self.fc = nn.Linear(hidden_size * direction_multiplier, output_size)
 
 
     def forward(self, x):
@@ -34,45 +30,23 @@ class MultiAssetLSTM(nn.Module):
         """
 
         out, _ = self.lstm(x)   # (batch_size, seq_len, lstm_out_size*direction_multiplier)
-        out = out[:, -1, :]     # (batch_size, lstm_out_size*direction_multiplier)
-        out = self.fc(out)      # (batch_size, num_tickers)
+        out = out[:, -1]        # (batch_size, lstm_out_size*direction_multiplier)
+        out = self.norm(out)
+        out = self.fc(out)      # (batch_size, output_size)
         return out
-    
-
-class MultiAssetBiLSTM(MultiAssetLSTM):
-    def __init__(self, input_size, hidden_size=64, num_layers=2, num_tickers=1):
-        super().__init__(
-            input_size=input_size, 
-            hidden_size=hidden_size,
-            num_layers=num_layers,
-            num_tickers=num_tickers,
-            bidirectional=True
-        )
 
 
-class MultiAssetProjectedLSTM(MultiAssetLSTM):
-    def __init__(self, input_size, hidden_size=64, num_layers=2, proj_size=32, num_tickers=1):
-
-       super().__init__(
-            input_size=input_size, 
-            hidden_size=hidden_size,
-            proj_size=proj_size,
-            num_layers=num_layers,
-            num_tickers=num_tickers,
-        )
-       
-
-class MultiAssetSeq2SeqLSTM(nn.Module):
+class Seq2SeqLSTM(nn.Module):
     def __init__(self,
                  input_size,
                  hidden_size=64,
                  num_layers=2,
                  horizon=10,
-                 num_tickers=2):
+                 output_size=1):
         super().__init__()
 
         self.horizon = horizon
-        self.num_tickers = num_tickers
+        self.output_size = output_size
 
         self.encoder_lstm = nn.LSTM(
             input_size=input_size,
@@ -82,13 +56,13 @@ class MultiAssetSeq2SeqLSTM(nn.Module):
         )
 
         self.decoder_lstm = nn.LSTM(
-            input_size=num_tickers,
+            input_size=output_size,
             hidden_size=hidden_size,
             num_layers=num_layers,
             batch_first=True
         )
 
-        self.fc = nn.Linear(hidden_size, num_tickers)
+        self.fc = nn.Linear(hidden_size, output_size)
 
     
     def forward(self, x):
@@ -96,38 +70,34 @@ class MultiAssetSeq2SeqLSTM(nn.Module):
          x: (batch_size, seq_len, input_size)
         """
         batch_size = x.size(0)
-        _, (hidden, cell) = self.encoder_lstm(x)  # (batch_size, num_layers, hidden_size)
+        _, (hidden, cell) = self.encoder_lstm(x)  # (num_layers, batch_size, hidden_size)
         decoder_input = torch.zeros(batch_size, 1, self.num_tickers).to(x.device)
         
         outputs = []
 
         for _ in range(self.horizon):
             out, (hidden, cell) = self.decoder_lstm(decoder_input, (hidden, cell))
-            decoder_input = self.fc(out)     # (batch_size, 1, num_tickers)
+            pred = self.fc(out)     # (batch_size, 1, output_size)
             
             outputs.append(decoder_input)
+            decoder_input = pred
 
-        outputs = torch.cat(outputs, dim=1) # (batch_size, horizon, num_tickers)
+        outputs = torch.cat(outputs, dim=1) # (batch_size, horizon, output_size)
 
         return outputs
 
 
-class MultiAssetSeq2SeqBiLSTM(MultiAssetSeq2SeqLSTM):
+class Seq2SeqBiLSTM(nn.Module):
     def __init__(self,
                  input_size,
                  hidden_size=64,
                  num_layers=2,
                  horizon=10,
-                 num_tickers=2):
-        super().__init__(
-            input_size,
-            hidden_size=64,
-            num_layers=2,
-            horizon=10,
-            num_tickers=2)
+                 output_size=1):
+        super().__init__()
 
         self.horizon = horizon
-        self.num_tickers = num_tickers
+        self.output_size = output_size
         self.num_layers = num_layers
         self.hidden_size = hidden_size
 
@@ -139,15 +109,21 @@ class MultiAssetSeq2SeqBiLSTM(MultiAssetSeq2SeqLSTM):
             bidirectional=True
         )
 
+        self.decoder_lstm = nn.LSTM(
+            input_size=output_size,
+            hidden_size=hidden_size,
+            num_layers=num_layers,
+            batch_first=True,
+            bidirectional=False
+        )
+
+        self.fc = nn.Linear(hidden_size*2, output_size)
+
     
     def _merge_states(self, x):
-        """
-         x: (batch_size, 2 * num_layers, hidden_size)
-        """
 
-        x = x.view(x.size(0), self.num_layers, 2, self.hidden_size)
-        x = x.sum(dim=1)    # (batch_size, num_layers, hidden_size)
-
+        x = x.view(self.num_layers, 2, x.size(1), self.hidden_size)
+        x = torch.cat((x[:, 0, :, :], x[:, 1, :, :]), dim=2)    # (num_layers, batch_size, hidden_size*2)
         return x
 
     def forward(self, x):
@@ -156,23 +132,21 @@ class MultiAssetSeq2SeqBiLSTM(MultiAssetSeq2SeqLSTM):
         """
 
         batch_size = x.size(0)
-        _, (hidden, cell) = self.encoder_lstm(x)  # (batch_size, 2*num_layers, hidden_size)
+        _, (hidden, cell) = self.encoder_lstm(x)  # (2*num_layers, batch_size, hidden_size)
 
         # Merge bidirectional states
-        hidden = self._merge_states(hidden)       # (batch_size, num_layers, hidden_size)
-        cell = self._merge_states(cell)           # (batch_size, num_layers, hidden_size)
+        hidden = self._merge_states(hidden)      # (num_layers, batch_size, hidden_size*2)
+        cell = self._merge_states(cell)          # (num_layers, batch_size, hidden_size*2)
         
-        decoder_input = torch.zeros(batch_size, 1, self.num_tickers).to(x.device)
+        decoder_input = torch.zeros(batch_size, 1, self.output_size, device= x.device)
         outputs = []
 
         for _ in range(self.horizon):
             out, (hidden, cell) = self.decoder_lstm(decoder_input, (hidden, cell))
-            decoder_input = self.fc(out)     # (batch_size, 1, num_tickers)
-            
+            pred = self.fc(out)     
             outputs.append(decoder_input)
+            decoder_input = pred
 
-        outputs = torch.cat(outputs, dim=1) # (batch_size, horizon, num_tickers)
+        outputs = torch.cat(outputs, dim=1) # (batch_size, horizon, output_dim)
 
         return outputs
-
-    
